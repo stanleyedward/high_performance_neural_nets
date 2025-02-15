a2cc62d4919968a845d825e9780a9f016dfb876e
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2025-02-15 13:36:10 +0530

    add verification function

--- src/activation_kernels.cuh
+++ src/activation_kernels.cuh
@@ -1,5 +1,5 @@
 #pragma once
 
-#include "activation_kernels/0_softmaxCPU.cu"
-#include "activation_kernels/1_softmax.cu"
-#include "activation_kernels/2_smemCoalUnrolledSoftmax.cu"
+#include "activation_kernels/0_softmaxCPU.cuh"
+#include "activation_kernels/1_softmax.cuh"
+#include "activation_kernels/2_smemCoalUnrolledSoftmax.cuh"
---
+++ src/activation_runner.cu
@@ -0,0 +1,53 @@
+#pragma once
+#include "activation_kernels.cuh"
+#include <cuda_runtime.h>
+#include <stdio.h>
+
+void run_softmaxCPU(int M, int N, float *input, float *output) {
+    softmax_cpu(M, N, input, output);
+}
+
+void run_softmax1(int M, int N, float *d_input, float *d_output) {
+    dim3 numThreadsPerBlock = dim3(32, 32, 1);
+    dim3 numBlocks = dim3((N + numThreadsPerBlock.x - 1) / numThreadsPerBlock.x, (M + numThreadsPerBlock.y - 1) / numThreadsPerBlock.y, 1);
+
+    softmax<<<numBlocks, numThreadsPerBlock>>>(M, N, d_input, d_output);
+
+}
+
+void run_softmax2(int M, int N, float *d_input, float *d_output) {
+    int BLOCK_DIM_Y = 1024;
+    dim3 block_size = dim3(1, BLOCK_DIM_Y, 1);
+    dim3 grid_size = dim3(M, 1, 1);  
+    smem_coal_unrolled_softmax<<<grid_size, block_size>>>(BLOCK_DIM_Y, M, N, d_input, d_output);
+}
+
+void verify_results(uint M, uint N, float *gpu_results1, float *gpu_results2, float *cpu_results){
+    // Verify results
+    float max_error1 = 0.0f;
+    float max_error2 = 0.0f;
+    for (int i = 0; i < M * N; i++) {
+        max_error1 = fmax(max_error1, fabs(gpu_results1[i] - cpu_results[i]));
+        max_error2 = fmax(max_error2, fabs(gpu_results2[i] - cpu_results[i]));
+    }
+    
+    printf("Max error 1: %f\n", max_error1);
+    printf("Max error 2: %f\n", max_error2);
+}
+
+void run_kernel_softmax(int kernel_num, int M, int N, float *d_input, float *d_output) {
+    switch (kernel_num) {
+        case 0:
+            run_softmaxCPU(M, N, d_input, d_output);
+            break;
+        case 1:
+            run_softmax1(M, N, d_input, d_output);
+            break;
+        case 2:
+            run_softmax2(M, N, d_input, d_output);
+            break;
+        default:
+            printf("Invalid kernel number\n");
+            break;
+    }
+}
