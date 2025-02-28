de60ab9f62b37be1367996f6253c0652bd9bedcb
stanleyedward <114278820+stanleyedward@users.noreply.github.com>
2025-03-01 01:01:39 +0530

    update kernel

--- src/mm_kernels/3_smemCaching.cuh
+++ src/mm_kernels/3_smemCaching.cuh
@@ -10,44 +10,29 @@ __global__ void mm3(uint M, uint N, uint K, float *A, float *B, float *C, float 
   const uint cRow = blockIdx.x;
   const uint cCol = blockIdx.y;
 
-  // allocate buffer for current block in fast shared mem
-  // shared mem is shared between all threads in a block
   __shared__ float As[BLOCK_SIZE * BLOCK_SIZE];
   __shared__ float Bs[BLOCK_SIZE * BLOCK_SIZE];
 
-  //  extern __shared__ float sdata[];
-  // float* As = sdata;
-  // float* Bs = sdata + BLOCK_SIZE * BLOCK_SIZE;
-
-  // the inner row & col that we're accessing in this thread
   const uint threadCol = threadIdx.x % BLOCK_SIZE;
   const uint threadRow = threadIdx.x / BLOCK_SIZE;
 
-  // advance pointers to the starting positions
   A += cRow * BLOCK_SIZE * K;                    // row=cRow, col=0
   B += cCol * BLOCK_SIZE;                        // row=0, col=cCol
   C += cRow * BLOCK_SIZE * N + cCol * BLOCK_SIZE; // row=cRow, col=cCol
 
   float tmp = 0.0;
   for (int bkIdx = 0; bkIdx < K; bkIdx += BLOCK_SIZE) {
-    // Have each thread load one of the elements in A & B
-    // Make the threadCol (=threadIdx.x) the consecutive index
-    // to allow global memory access coalescing
     As[threadRow * BLOCK_SIZE + threadCol] = A[threadRow * K + threadCol];
     Bs[threadRow * BLOCK_SIZE + threadCol] = B[threadRow * N + threadCol];
 
-    // block threads in this block until cache is fully populated
     __syncthreads();
     A += BLOCK_SIZE;
     B += BLOCK_SIZE * N;
 
-    // execute the dotproduct on the currently cached block
     for (int dotIdx = 0; dotIdx < BLOCK_SIZE; ++dotIdx) {
       tmp += As[threadRow * BLOCK_SIZE + dotIdx] *
              Bs[dotIdx * BLOCK_SIZE + threadCol];
     }
-    // need to sync again at the end, to avoid faster threads
-    // fetching the next block into the cache before slower threads are done
     __syncthreads();
   }
   C[threadRow * N + threadCol] = bias[cCol * BLOCK_SIZE + threadCol] + tmp;
